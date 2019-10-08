f562e2cbd67b2e8c6cf72ba6d93fec73da849906
Melody Nicholls <m.nicholls3@lancaster.ac.uk>
2019-10-08 01:08:02 +0100

    no idea why this is happening. Very frustrating

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express')
-const requireApi = require('./middleware/RequireApi')
 const dotenv = require('dotenv')
+const generateDB = require('./database/builder')
+
+const requireApi = require('./middleware/RequireApi')
 
 const app = express()
 dotenv.config()
 
 app.use(requireApi)
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// > TODO: Implement users route
+// > TODO: Implement videos route and database
+// > TODO: Implement playlists route
+
+app.post('/database/builder', (req, res) => {
+    generateDB();
+    res.send("Generating DB")
 })
 
 app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
+    console.log('Example app listening on port 8000!')
 })
