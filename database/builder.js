424828c40080701d9b57c386f923036d75a2f90c
Melody Nicholls <m.nicholls3@lancaster.ac.uk>
2019-10-08 01:07:18 +0100

    Began implementing database builders

---
+++ database/builder.js
@@ -0,0 +1,12 @@
+const mysql = require("./connect")
+
+function generateDatabase() {
+    // mysql.query(
+    //     "CREATE SCHEMA IF NOT EXISTS ${process.env.DATABASE};" +
+    //     "CREATE TABLE `la1tv`.`APIKEys` ( `ID` INT NOT NULL AUTO_INCREMENT , `Key` VARCHAR(32) NOT NULL , PRIMARY KEY (`ID`), UNIQUE (`Key`)) ENGINE = InnoDB;"
+    // )
+
+    console.log("In development")
+}
+
+module.exports = generateDatabase
