bbb5c9edd6164b0c8fab5e3b9315601601364eb1
Melody Nicholls <m.nicholls3@lancaster.ac.uk>
2019-10-08 02:00:43 +0100

    moved api-key to folder

---
+++ database/api-key/create.js
@@ -0,0 +1,14 @@
+const mysql = require('./connect')
+const uuid = require('uuid/v4');
+
+const createKey = async() => {
+    let id = uuid().toString().replace(/-/g, '')
+    let SQL = "INSERT INTO `api-keys` (`ID`, `api-key`) VALUES (NULL, '" + id + "');"
+    console.log(SQL + "    " + id)
+    mysql.promise()
+        .query(SQL)
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err))
+}
+
+module.exports = createKey
