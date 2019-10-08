515537f5ac3b87d717a108b71b6d639260217c34
Melody Nicholls <m.nicholls3@lancaster.ac.uk>
2019-10-08 01:07:41 +0100

    Fixed some typos

--- database/connect.js
+++ database/connect.js
@@ -1,13 +1,25 @@
 const mysql = require('mysql2');
+const dotenv = require('dotenv')
+dotenv.config()
 
 // > TODO: Test the connection
 // > TODO: Create database creation scripts
 // > TODO: Create seeders
 
+const data = {
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+}
+
+console.log(data)
+
 const pool = mysql.createPool({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USERNAME,
-    database: process.env.DATABASE_PASSWORD,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
