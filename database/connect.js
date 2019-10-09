c9ef82a385d91b038533a7f7748d97e5cb260ea5
Melody Nicholls <m.nicholls3@lancaster.ac.uk>
2019-10-09 22:05:41 +0100

    Swapped over to config.json

--- database/connect.js
+++ database/connect.js
@@ -1,16 +1,16 @@
 const mysql = require('mysql2');
-const dotenv = require('dotenv')
-dotenv.config()
+const { DATABASE } = require('../config/config.json')
 
 // > TODO: Test the connection
 // > TODO: Create database creation scripts
 // > TODO: Create seeders
 
 const pool = mysql.createPool({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE,
+    host: DATABASE.HOST | '127.0.0.1',
+    user: DATABASE.USERNAME,
+    password: DATABASE.PASSWORD,
+    database: DATABASE.DATABASE,
+    port: DATABASE.PORT | 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
