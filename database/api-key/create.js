4e5027bd026b02892de507daab29a5e87089f262
Melody Nicholls <m.nicholls3@lancaster.ac.uk>
2019-10-10 02:25:07 +0100

    Implemented API key authorisation middleware

--- database/api-key/create.js
+++ database/api-key/create.js
@@ -1,13 +1,19 @@
 const mysql = require('../connect')
 const uuid = require('uuid/v4');
 
-const createKey = async() => {
-    let id = uuid().toString().replace(/-/g, '')
-    let SQL = "INSERT INTO `api-keys` (`ID`, `api-key`) VALUES (NULL, '" + id + "');"
-    mysql.promise()
-        .query(SQL)
-        .then((data) => console.log(data))
-        .catch((err) => console.log(err))
+const createKey = async(req, res) => {
+    const id = uuid().toString().replace(/-/g, '')
+    mysql.promise().execute('INSERT INTO `api_keys` (`ID`, `api_key`, `enabled`, `view_vod_uri`, `view_stream_uri`, `use_webhook`) VALUES (NULL, ?, ?, ?, ?, ?)', [id, 1, 1, 1, 1])
+        .then(() => {
+            res.status(201).json({
+                data: id
+            })
+        })
+        .catch(() => {
+            res.status(409).json({
+                error: err
+            })
+        })
 }
 
 module.exports = createKey
--- middleware/RequireApi.js
+++ middleware/RequireApi.js
@@ -1,4 +1,5 @@
-const mysql = require('../database/connect')
+const getAPI = require('../database/api-key/get')
+
 
 // > TODO: Implement checking from database for valid API keys
 // > TODO: Add header to incoming request that filters for adding and / or removing
@@ -7,12 +8,19 @@ const mysql = require('../database/connect')
 const requireAPI = (req, res, next) => {
     if (!req.get("x-api-key")) {
         console.log("No X-API-KEY sent");
-        res.send("Invalid or No API Key provided");
+        res.status(401).send("Invalid or No API Key provided");
 
         return;
     }
 
-    next();
+    getAPI(req.get("x-api-key")).then((result) => {
+        console.log(JSON.parse(result))
+        if (JSON.parse(result).enabled === 1) {
+            next();
+        } else {
+            res.status(401).send("Invalid or No API Key provided");
+        }
+    })
 }
 
 module.exports = requireAPI
