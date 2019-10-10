fabdddb8c070cedc2e4a9bc384a8738d1f5d3dd2
Melody Nicholls <m.nicholls3@lancaster.ac.uk>
2019-10-10 18:27:16 +0100

    Swapped createKey to a promise that is not Async (will force to wait for data)

--- database/api-key/create.js
+++ database/api-key/create.js
@@ -1,19 +1,17 @@
 const mysql = require('../connect')
 const uuid = require('uuid/v4');
 
-const createKey = async(req, res) => {
-    const id = uuid().toString().replace(/-/g, '')
-    mysql.promise().execute('INSERT INTO `api_keys` (`ID`, `api_key`, `enabled`, `view_vod_uri`, `view_stream_uri`, `use_webhook`) VALUES (NULL, ?, ?, ?, ?, ?)', [id, 1, 1, 1, 1])
-        .then(() => {
-            res.status(201).json({
-                data: id
+const createKey = ({ enabled = 1, view_vod_uri = 0, view_stream_uri = 0, use_webhook = 0 }) => {
+    return new Promise((resolve, reject) => {
+        const id = uuid().toString().replace(/-/g, '')
+        mysql.promise().execute('INSERT INTO `api_keys` (`ID`, `api_key`, `enabled`, `view_vod_uri`, `view_stream_uri`, `use_webhook`) VALUES (NULL, ?, ?, ?, ?, ?)', [id, enabled, view_vod_uri, view_stream_uri, use_webhook])
+            .then(() => {
+                resolve(id)
             })
-        })
-        .catch(() => {
-            res.status(409).json({
-                error: err
+            .catch((err) => {
+                reject(err)
             })
-        })
+    })
 }
 
 module.exports = createKey
