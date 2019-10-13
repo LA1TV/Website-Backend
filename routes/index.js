0953b132e5442a588ce12a476a12ed6690d4eb88
Melody Nicholls <m.nicholls3@lancaster.ac.uk>
2019-10-13 14:05:20 +0100

    Implemented Kristian's suggestion of removing URL Hyphens

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-router.use('/api-key', require('./api-key'))
+router.use('/api/key', require('./api-key'))
 router.use('/videos', require('./videos'))
 router.use('/playlist', require('./playlist'))
 
