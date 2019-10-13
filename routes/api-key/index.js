7001c5c5a990c0356b7ab3bb8aac4dc33c4440b0
Melody Nicholls <m.nicholls3@lancaster.ac.uk>
2019-10-13 12:44:53 +0100

    Added default API-Key routing

--- routes/api-key/index.js
+++ routes/api-key/index.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const router = express.Router()
 
-const generateAPI = require('../../database/api-key/create')
+const { createKey, getAllKeys, getSpecifiedKey } = require('../../database/api-key')
 
-router.post('/create', (req, res) => {
-    generateAPI(req, res)
+router.post('/', (req, res) => {
+    createKey({}).then(returnValues => {
+            res.status(200).json({ data: returnValues })
+        })
+        .catch(err => {
+            res.status(500).json({ error: "Creation of new API key failed" })
+        })
+})
+
+router.get('/', (req, res) => {
+    getAllKeys({}).then(data => {
+            res.status(200).json(data)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: "Requesting all keys failed!" })
+        })
 })
 
 module.exports = router
