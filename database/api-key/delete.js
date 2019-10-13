91f434c0d00da62c5ec01e27f293b89765a73a7f
Melody Nicholls <m.nicholls3@lancaster.ac.uk>
2019-10-13 14:22:18 +0100

    Added deleteKey function

--- database/api-key/delete.js
+++ database/api-key/delete.js
@@ -1,7 +1,15 @@
 const mysql = require('../connect')
 
 const deleteKey = (api) => {
-
+    return new Promise((resolve, reject) => {
+        mysql.promise().execute('DELETE FROM `api_keys` WHERE api_key = ?', [api])
+            .then(([fields, rows]) => {
+                resolve("success")
+            })
+            .catch(err => {
+                reject(err)
+            })
+    })
 }
 
 module.exports = {
