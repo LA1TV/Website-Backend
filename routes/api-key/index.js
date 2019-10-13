df58b13085571bbf37cfc2bd79072da5c0e7a06d
Melody Nicholls <m.nicholls3@lancaster.ac.uk>
2019-10-13 15:34:43 +0100

    Minor layout changes

--- routes/api-key/index.js
+++ routes/api-key/index.js
@@ -3,22 +3,36 @@ const router = express.Router()
 
 const { createKey, getAllKeys, getSpecifiedKey } = require('../../database/api-key')
 
+router.get('/', (req, res) => {
+    getAllKeys({})
+        .then(data => {
+            res.status(200).json(data)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: "Requesting all keys failed!" })
+        })
+})
+
+router.get('/:id', (req, res) => {
+    getSpecifiedKey(req.params.id)
+        .then(data => {
+            res.status(200).json(data)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: "Requesting specified keys " + req.params.id + "failed!"
+            })
+        })
+})
+
 router.post('/', (req, res) => {
     createKey({}).then(returnValues => {
             res.status(200).json({ data: returnValues })
         })
         .catch(err => {
             res.status(500).json({ error: "Creation of new API key failed" })
-        })
-})
-
-router.get('/', (req, res) => {
-    getAllKeys({}).then(data => {
-            res.status(200).json(data)
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).json({ error: "Requesting all keys failed!" })
         })
 })
 
