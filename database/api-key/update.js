7c320526251269d408c921a0500771010ebadeb8
Melody Nicholls <m.nicholls3@lancaster.ac.uk>
2019-10-14 17:24:46 +0100

    Allowed the RequireApi middleware to update last_request_at value for VALID api keys

--- database/api-key/update.js
+++ database/api-key/update.js
@@ -20,18 +20,12 @@ const updateKey = (api, { enabled, view_vod_uri, view_stream_uri, use_webhook })
     })
 }
 
+// > Updates the timestamp for the API key
+// > Is mainly used in the middleware RequireApi.js
 const updateTimeStamp = (api) => {
     return new Promise((resolve, reject) => {
-        let enabledSQL = enabled ? ('`enabled` = ' + enabled) : ('')
-        let view_vod_uri_SQL = view_vod_uri ? ('`view_vod_uri` = ' + view_vod_uri) : ('')
-        let view_stream_uri_SQL = view_stream_uri ? ('`view_stream_uri` = ' + view_stream_uri) : ('')
-        let use_webhook_SQL = use_webhook ? ('`use_webhook` = ' + use_webhook) : ('')
-
-        mysql.promise().execute('UPDATE api_keys SET ' + enabledSQL +
-                ' ' + view_vod_uri_SQL +
-                ' ' + view_stream_uri_SQL +
-                ' ' + use_webhook_SQL +
-                ' WHERE `api_key` = ?;', [api]).then(([fields, rows]) => {
+        mysql.promise().execute('UPDATE api_keys SET last_request_at = CURRENT_TIMESTAMP WHERE `api_key` = ?;', [api])
+            .then(([fields, rows]) => {
                 resolve("success")
             })
             .catch(err => {
@@ -41,5 +35,6 @@ const updateTimeStamp = (api) => {
 }
 
 module.exports = {
-    updateKey
+    updateKey,
+    updateTimeStamp
 }
--- middleware/RequireApi.js
+++ middleware/RequireApi.js
@@ -1,5 +1,5 @@
 const { getSpecifiedKey } = require('../database/api-key/get')
-
+const { updateTimeStamp } = require('../database/api-key/update')
 
 // > TODO: Implement checking from database for valid API keys
 // > TODO: Add header to incoming request that filters for adding and / or removing
@@ -14,7 +14,13 @@ const requireAPI = (req, res, next) => {
 
     getSpecifiedKey(req.get("x-api-key")).then((result) => {
             if (JSON.parse(result).enabled === 1) {
-                next();
+                updateTimeStamp(req.get("x-api-key")).then(data => {
+                    next()
+                }).catch(err => {
+                    console.log(err)
+                    res.status(401).send("Unable to update timestamp of API key provided")
+                    return
+                })
             } else {
                 res.status(401).send("Invalid or No API Key provided")
                 return
